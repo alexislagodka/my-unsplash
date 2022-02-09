f14a3f965fde7fa24f95276ef46361c68d8129a9
Alexis LAGODKA <alexis.lagodka@bethunebruay.fr>
2022-02-09 16:51:31 +0100

    modal transition

--- components/Admin/AdminMenu.js
+++ components/Admin/AdminMenu.js
@@ -4,14 +4,13 @@ export default function AdminMenu ({ user, logout, changeEmail, changePassword, 
   return (
     <div className='grid'>
       <div className='flex justify-between items-center pb-2 overflow-auto'>
-        <h2>Hello &apos;{user && user.email}&apos;</h2>
-        <button className='closeAdminButton' onClick={() => handleCancel()}>X</button>
+        <h2>Hello {user && user.email}</h2>
       </div>
       <div className='grid gap-2'>
-        <button className='adminActionButton' onClick={logout}>Log out</button>
-        <button className='adminActionButton' onClick={changeEmail}>Change email</button>
-        <button className='adminActionButton' onClick={changePassword}>Change password</button>
-        <button className='adminActionButton' onClick={deleteAccount}>Delete account</button>
+        <button className='adminActionButton' onClick={logout}>&#xE9BA; Log out</button>
+        <button className='adminActionButton' onClick={changeEmail}>&#xE0BE; Change email</button>
+        <button className='adminActionButton' onClick={changePassword}>&#xF042; Change password</button>
+        <button className='adminActionButton' onClick={deleteAccount}>&#xE510; Delete account</button>
       </div>
     </div>
   )
